be6ad597ead788de7d40de179a4ee8d77b751536
askmsj <s.jelen@wp.pl>
2019-09-08 13:14:17 +0200

    update and add config

--- device_info.py
+++ device_info.py
@@ -1,6 +1,27 @@
+import os
+
+def getConfig():
+    out = {}
+    try:
+        #print(os.getcwd())        
+        with open('config', 'r') as tFile:
+            for line in tFile:
+                #print(line,line.startswith('#'))
+                if not line.startswith('#'):
+                    argval = line.rstrip('\n').split("=")
+                    arg = argval[0]
+                    val = argval[1]
+                    out[arg] = val
+        
+        return out
+
+    except:
+        return {}
+
+    return {}
+
 
 def getTemperature():
-    
     try:
         tFile = open('/sys/class/thermal/thermal_zone0/temp', 'r')
         temp = float(tFile.read())
@@ -11,6 +32,45 @@ def getTemperature():
         tFile.close()
 
     return None   
+
+
+def getVolts():
+    f = os.popen('/opt/vc/bin/vcgencmd measure_volts').read()
+    #f = os.system("/opt/vc/bin/vcgencmd measure_volts")
+    out = f.split('=')[1].replace('V','')
+    return out
+
+def getThrottled():
+#    1110000000000000010
+#    |||             |||_ under-voltage
+#    |||             ||_ currently throttled
+#    |||             |_ arm frequency capped
+#    |||_ under-voltage has occurred since last reboot!!
+#    ||_ throttling has occurred since last reboot
+#    |_ arm frequency capped has occurred since last reboot!!
+  
+    MESSAGES = {
+        0: 'Under-voltage!',
+        1: 'ARM frequency capped!',
+        2: 'Currently throttled!',
+        3: 'Soft temperature limit ON',
+        16: 'Under-voltage has occurred since last reboot.',
+        17: 'Throttling has occurred since last reboot.',
+        18: 'ARM frequency capped has occurred since last reboot.',
+        19: 'Soft temperature limit has occurred since last reboot.',
+    }
+    
+    f = os.popen('/opt/vc/bin/vcgencmd get_throttled').read()
+    throttled_binary = bin(int(f.split('=')[1], 0))
+    #print(throttled_binary)
+
+    warnings = 0
+    for position, message in MESSAGES.items():
+        if len(throttled_binary) > position and throttled_binary[0 - position - 1] == '1':
+            print(message)
+            warnings += 1
+    
+    return f
 
 def getSerial():
     res = None
@@ -26,4 +86,22 @@ def getSerial():
     except:
         tFile.close()
 
-    return res             
+    return res
+
+
+def getJsonData(gateId):
+    out = dict(gateId=gateId)
+    out['temp'] = getTemperature()
+    out['volts'] = getVolts()
+    out['throttled'] = getThrottled()
+    
+    return out
+
+def main():
+    #print(bin(0x80008))
+    print(getVolts())
+    
+    
+if __name__ == "__main__":
+    main()
+
--- url_requests.py
+++ url_requests.py
@@ -1,5 +1,10 @@
 # importing the requests library 
-import requests 
+import requests
+import json
+from pprint import pprint
+from urllib.parse import urlencode
+
+baseURL = "http://192.168.0.199/index.php/api/"
 
 def wait_for_internet_connection():
     while True:
@@ -13,6 +18,7 @@ def get(_url, _params = {}):
     PARAMS = _params
     #PARAMS = {'address': 'dehli technological university'}
     #_url = "http://maps.googleapis.com/maps/api/geocode/json"
+    
     r = requests.get(url = _url, params = PARAMS)
     #print(r)
     return r
@@ -22,13 +28,20 @@ def get(_url, _params = {}):
     
 def post(_url, _params = {}):
     
+    url = (baseURL if baseURL.endswith('/') else baseURL + '/') + _url
+    
+    if baseURL == "":
+        return None
+    
+    headers = {'content-type': 'application/json'}
+    
     # defining the api-endpoint  
     #API_ENDPOINT = "http://pastebin.com/api/api_post.php"  
     # your API key here 
     #API_KEY = "XXXXXXXXXXXXXXXXX"
     
     # data to be sent to api
-    data = {}
+    _data = json.dumps(_params)
     #data = {'api_dev_key':API_KEY, 
     #    'api_option':'paste',
     #    'api_paste_format':'python'} 
@@ -36,21 +49,46 @@ def post(_url, _params = {}):
     #json data?
     #json.dumps(data)
     # sending post request and saving response as response object
+    #s = requests.Session()
+    
     r = None
     try:
-        r = requests.post(url = _url, data = data)
-                
+        #print('ttt',json.dumps(_params))      
+        print(type(_params))
+        print(url, _params, _data)
+        r = requests.post(url = url, data = {'data': _data})
+        print(r)
+        print(r.headers['content-type'])
+        print(r.content)
     except ValueError:
         r = None
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.ConnectionError as e2:
+        print(e2)
         r = None
+    except requests.exceptions.RequestException as e3:  # This is the correct syntax
+        print(e3)
+        sys.exit(1)
     except Exception as e:
+        print(e)
         r = None
-    
+        
+        
     #print(r)
     return r
-    # extracting response text  
-    #pastebin_url = r.text 
-    #print("The pastebin URL is:%s"%pastebin_url) 
+
+
+def main():
+    out = dict(gateId=71,temp=77)
+    #out['gateId'] = 71
+    #out['temp'] = 78
+    print(out, json.dumps(out))
+    baseURL = "http://192.168.0.199/index.php/api/"
+    post("device/" + str(112233) + "/q", out)
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
   
 
